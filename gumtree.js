fec981b03f993b5df4bbcc735d2787cd0cdb8c86
Filip Hnízdo <filip@bluejumpers.com>
2017-10-07 19:58:23 +0100

    don't error out on gumtree

--- gumtree.js
+++ gumtree.js
@@ -11,7 +11,7 @@ module.exports = {
 
     return new Promise(function(resolve, reject) {
 
-      request.get('https://www.gumtree.com/search?search_category=all&q=' + query.search, {}, function(err, res, body) {
+      request.get('https://www.gumtree.com/search?search_category=all&q=' + encodeURIComponent(query.search), {}, function(err, res, body) {
 
         const dom = new JSDOM(body, {
           includeNodeLocations: true
@@ -23,15 +23,20 @@ module.exports = {
 
         for (i = 0; i < ads.length; i++) {
 
-          results.push({
-            title: ads[i].getElementsByClassName("listing-title")[0].textContent.trim(),
-            location: ads[i].getElementsByClassName("listing-location")[0].textContent.trim(),
-            description: ads[i].getElementsByClassName("listing-description")[0].textContent.trim(),
-            price: ads[i].getElementsByClassName("listing-price")[0].textContent.trim(),
-            image: ads[i].getElementsByTagName("img")[0] ? ads[i].getElementsByTagName("img")[0].getAttribute("data-lazy") : undefined,
-            url: ads[i].getElementsByClassName("listing-link")[0] ? ads[i].getElementsByClassName("listing-link")[0].getAttribute("href") : undefined,
-            timestamp: Date.now()
-          });
+          if (ads[i].getElementsByClassName("listing-title")[0]) {
+
+            results.push({
+              title: ads[i].getElementsByClassName("listing-title")[0].textContent.trim(),
+              location: ads[i].getElementsByClassName("listing-location")[0].textContent.trim(),
+              description: ads[i].getElementsByClassName("listing-description")[0].textContent.trim(),
+              price: ads[i].getElementsByClassName("listing-price")[0].textContent.trim(),
+              image: ads[i].getElementsByTagName("img")[0] ? ads[i].getElementsByTagName("img")[0].getAttribute("data-lazy") : undefined,
+              url: ads[i].getElementsByClassName("listing-link")[0] ? ads[i].getElementsByClassName("listing-link")[0].getAttribute("href") : undefined,
+              timestamp: Date.now()
+            });
+
+          }
+
         }
 
         // Trim nonsense from start
